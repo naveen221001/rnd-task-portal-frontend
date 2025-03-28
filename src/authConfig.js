731df7f646b831b2dcd15398f09278b1ca15ad74
Naveen Kumar Chamaria <naveen.c@vspl.com>
2025-03-28 21:41:39 +0530

    fix: use dynamic redirectUri for MSAL

--- src/authConfig.js
+++ src/authConfig.js
@@ -4,6 +4,7 @@ export const msalConfig = {
       clientId: "b52a8ad4-85a3-4095-99ea-51038c4a7b68",
       authority: "https://login.microsoftonline.com/69eaf322-247b-4276-925b-427c5af8d5c3",
       redirectUri: "https://rnd-task-portal.vercel.app/",
+      //
     }
   };
   
